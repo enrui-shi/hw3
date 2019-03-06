b6c6af187e47268b5544a4dbe49b465c37290fe2
stone <enrui.shi@stonybrook.edu>
2019-03-06 18:51:13 -0500

    add /speak

--- routers/speak.js
+++ routers/speak.js
@@ -14,8 +14,12 @@ router.post('/',jsonParser,function(req,res){
     data = req.body;
     msg = data.msg;
     key = data.key;
-    ch.publish('hw3', key, new Buffer(msg));
-    res.send(msg+" send to "+key);
+    amqp.connect('amqp://localhost', function(err, conn) {
+        conn.createChannel(function(err, ch) {
+            ch.publish('hw3', key, new Buffer(msg));
+            res.send(msg+" send to "+key);
+        });
+    });
 });
 
 //export this router to use in our index.js
